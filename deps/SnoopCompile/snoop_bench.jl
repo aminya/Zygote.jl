ce674e8db5c6a7460c1ae292752274979f36becf
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-05-31 04:00:47 -0500

    update scripts

--- deps/SnoopCompile/snoop_bench.jl
+++ deps/SnoopCompile/snoop_bench.jl
@@ -3,21 +3,21 @@ using SnoopCompile
 botconfig = BotConfig(
   "Zygote";
   os = ["linux", "windows", "macos"],
-  version = [v"1.4.1", v"1.3.1"],
+  version = [v"1.4.2", v"1.3.1"],
   blacklist = ["SqEuclidean"],
   exhaustive = false,
 )
 
 
 println("Benchmarking the inference time of `using Zygote`")
-snoopi_bench(
+snoop_bench(
   botconfig,
   :(using Zygote),
 )
 
 
 println("Benchmarking the inference time of `using Zygote` & basic function test")
-snoopi_bench(
+snoop_bench(
   botconfig,
   "$(@__DIR__)/example_script.jl",
 )
--- deps/SnoopCompile/snoop_bot.jl
+++ deps/SnoopCompile/snoop_bot.jl
@@ -3,12 +3,12 @@ using SnoopCompile
 botconfig = BotConfig(
   "Zygote";
   os = ["linux", "windows", "macos"],
-  version = [v"1.4.1", v"1.3.1"],
+  version = [v"1.4.2", v"1.3.1"],
   blacklist = ["SqEuclidean"],
   exhaustive = false,
 )
 
-snoopi_bot(
+snoop_bot(
   botconfig,
   "$(@__DIR__)/example_script.jl",
 )
